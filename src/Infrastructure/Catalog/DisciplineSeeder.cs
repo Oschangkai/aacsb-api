24c3127622934bd4d8a6495ab80d6fedf2024378
Oschangkai <oschangkai@kaiyeee.tw>
2022-10-04 22:57:19 +0800

    Database: Add Discipline seed data

--- src/Infrastructure/Catalog/DisciplineSeeder.cs
+++ src/Infrastructure/Catalog/DisciplineSeeder.cs
@@ -1,6 +1,45 @@
+using System.Reflection;
+using AACSB.WebApi.Application.Common.Interfaces;
+using AACSB.WebApi.Domain.Catalog;
+using AACSB.WebApi.Infrastructure.Persistence.Context;
+using AACSB.WebApi.Infrastructure.Persistence.Initialization;
+using Microsoft.Extensions.Logging;
+
 namespace AACSB.WebApi.Infrastructure.Catalog;
 
-public class DisciplineSeeder
+public class DisciplineSeeder : ICustomSeeder
 {
-    
+    private readonly ISerializerService _serializerService;
+    private readonly ApplicationDbContext _db;
+    private readonly ILogger<DisciplineSeeder> _logger;
+
+    public DisciplineSeeder(ISerializerService serializerService, ILogger<DisciplineSeeder> logger, ApplicationDbContext db)
+    {
+        _serializerService = serializerService;
+        _logger = logger;
+        _db = db;
+    }
+
+    public async Task InitializeAsync(CancellationToken cancellationToken)
+    {
+        string? path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        if (_db.Disciplines.Any()) return;
+
+        _logger.LogInformation("Started to Seed Discipline.");
+
+        string disciplineData = await File.ReadAllTextAsync(path + "/Catalog/discipline.json", cancellationToken);
+
+        var disciplines = _serializerService.Deserialize<List<Discipline>>(disciplineData);
+
+        if (disciplines is { Count: > 0 })
+        {
+            foreach (var discipline in disciplines)
+            {
+                await _db.Disciplines.AddAsync(discipline, cancellationToken);
+            }
+        }
+
+        await _db.SaveChangesAsync(cancellationToken);
+        _logger.LogInformation("Seeded Discipline.");
+    }
 }
