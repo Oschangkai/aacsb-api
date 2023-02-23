625e97cef0c9765459c58feef10c168b887f0d95
Oschangkai <oschangkai@kaiyeee.tw>
2023-02-23 15:38:58 +0800

    func: course query request (#5)

---
+++ tests/Infrastructure.Test/Common/Request/CourseQueryRequestTest.cs
@@ -0,0 +1,33 @@
+using AACSB.WebApi.Infrastructure.Common.Request;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Infrastructure.Test.Common.Request;
+
+public class CourseQueryRequestTest
+{
+    private readonly ITestOutputHelper _output;
+    private readonly CourseQueryRequest _query;
+
+    public CourseQueryRequestTest(ITestOutputHelper output)
+    {
+        _output = output;
+
+        var logger = Mock.Of<ILogger<CourseQueryRequest>>();
+        _query = new CourseQueryRequest(logger);
+    }
+
+    [Fact]
+    public async void GetCourseByDepartment()
+    {
+        var result = await _query.GetCourseByDepartment("1091", "CS", CancellationToken.None);
+
+        // _output.WriteLine(await result.ChCourses);
+        // _output.WriteLine(await result.EnCourses);
+
+        Assert.NotEmpty(await result.ChCourses);
+        Assert.NotEmpty(await result.EnCourses);
+    }
+}
---
+++ tests/Infrastructure.Test/ReportGenerator/CourseQuery.cs
@@ -0,0 +1,8 @@
+using Xunit;
+
+namespace Infrastructure.Test.ReportGenerator;
+
+public class CourseQueryRequestTest
+{
+
+}
