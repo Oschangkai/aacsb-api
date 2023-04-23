78d96d0523fc01fcfdc8d92dd142617a69dabae3
Oschangkai <oschangkai@kaiyeee.tw>
2023-04-23 23:40:54 +0800

    Feat: fetch course job

--- src/Infrastructure/ReportGenerator/FetchCoursesJob.cs
+++ src/Infrastructure/ReportGenerator/FetchCoursesJob.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using AACSB.WebApi.Application.Common.Interfaces;
 using AACSB.WebApi.Application.Common.Persistence;
 using AACSB.WebApi.Application.ReportGenerator;
@@ -11,7 +12,6 @@ using Hangfire;
 using Hangfire.Console.Extensions;
 using Hangfire.Console.Progress;
 using Hangfire.Server;
-using MediatR;
 using Microsoft.Extensions.Logging;
 
 namespace AACSB.WebApi.Infrastructure.ReportGenerator;
@@ -19,11 +19,10 @@ namespace AACSB.WebApi.Infrastructure.ReportGenerator;
 public class FetchCoursesJob : IFetchCourseJob
 {
     private readonly ILogger<FetchCoursesJob> _logger;
-    private readonly ISender _mediator;
-    private readonly IRepositoryWithEvents<Course> _courseRepo;
-    private readonly IRepositoryWithEvents<Teacher> _teacherRepo;
+    private readonly IRepository<Course> _courseRepo;
+    private readonly IRepository<Teacher> _teacherRepo;
     private readonly IReadRepository<Department> _departmentRepo;
-    private readonly IRepositoryWithEvents<ImportSignature> _importSignatureRepo;
+    private readonly IRepository<ImportSignature> _importSignatureRepo;
     private readonly IProgressBarFactory _progressBar;
     private readonly PerformingContext _performingContext;
     private readonly INotificationSender _notifications;
@@ -31,18 +30,16 @@ public class FetchCoursesJob : IFetchCourseJob
     private readonly IProgressBar _progress;
     public FetchCoursesJob(
         ILogger<FetchCoursesJob> logger,
-        ISender mediator,
-        IRepositoryWithEvents<Course> courseRepo,
-        IRepositoryWithEvents<Teacher> teacherRepo,
+        IRepository<Course> courseRepo,
+        IRepository<Teacher> teacherRepo,
         IReadRepository<Department> departmentRepo,
-        IRepositoryWithEvents<ImportSignature> importSignatureRepo,
+        IRepository<ImportSignature> importSignatureRepo,
         IProgressBarFactory progressBar,
         PerformingContext performingContext,
         INotificationSender notifications,
         ICurrentUser currentUser)
     {
         _logger = logger;
-        _mediator = mediator;
         _courseRepo = courseRepo;
         _teacherRepo = teacherRepo;
         _departmentRepo = departmentRepo;
@@ -95,7 +92,7 @@ public class FetchCoursesJob : IFetchCourseJob
 
         foreach (var dept in departments)
         {
-            var department = await _departmentRepo.GetBySpecAsync(new GetDepartmentByAbbrSpec(dept), cancellationToken);
+            var department = await _departmentRepo.FirstOrDefaultAsync(new GetDepartmentByAbbrSpec(dept), cancellationToken);
             _logger.LogInformation($"Fetch courses: {year}{semester}, {dept} with signature {importSignature.Name}");
 
             // Task3.1: Fetch Chinese and English course
@@ -106,40 +103,95 @@ public class FetchCoursesJob : IFetchCourseJob
             // Task3.2: Merge Chinese and English course
             var courses =
                 from cc in chineseCourses
-                join ec in englishCourses on cc.CourseNo equals ec.CourseNo
-                select new Course()
-                {
-                    Semester = Convert.ToDecimal($"{year}{semester}"),
-                    Code = cc.CourseNo,
-                    Name = cc.CourseName,
-                    EnglishName = ec.CourseName,
-                    DepartmentId = department.Id,
-                    Credit = Convert.ToDecimal(cc.CreditPoint),
-                    Required = cc.RequireOption == "R", // Required = R, Not required = E
-                    Year = cc.AllYear == "F", // Full Year = F, Half Year = H
-                    Time = cc.CourseTimes,
-                    ClassRoomNo = cc.ClassRoomNo,
-                    Contents = cc.Contents,
-                    ImportSignatureId = signature.Id,
-                    Teachers = cc.CourseTeacher.Contains(',')
-                        ? cc.CourseTeacher.Split(',')
-                            .Select((t, i) => new Teacher()
-                            {
-                                Name = t,
-                                EnglishName = ec.CourseTeacher.Split(',')[i]
-                            }).ToList()
-                        : new List<Teacher>() { new Teacher() { Name = cc.CourseTeacher, EnglishName = ec.CourseTeacher} }
-                };
+                    join ec in englishCourses on cc.CourseNo equals ec.CourseNo
+                    select new
+                    {
+                        Semester = Convert.ToDecimal($"{year}{semester}"),
+                        Code = cc.CourseNo,
+                        Name = cc.CourseName,
+                        EnglishName = ec.CourseName,
+                        DepartmentId = department.Id,
+                        Credit = Convert.ToDecimal(cc.CreditPoint),
+                        Required = cc.RequireOption == "R", // Required = R, Not required = E
+                        Year = cc.AllYear == "F", // Full Year = F, Half Year = H
+                        Time = cc.CourseTimes,
+                        ClassRoomNo = cc.ClassRoomNo,
+                        Contents = cc.Contents,
+                        ImportSignatureId = signature.Id,
+                        Teachers = new List<Teacher>()
+                        {
+                            new Teacher() { Name = cc.CourseTeacher.Trim(), EnglishName = ec.CourseTeacher.Trim(), EnglishNameInNtustCourse = ec.CourseTeacher.Trim() }
+                        },
+                    };
+            /*
+             * Task3.3: Split teacher name & Create teacher if not exist
+             * If teacher is multiple, split by comma
+             * Create teacher if not exist otherwise link to existing teacher
+             */
 
-            var cl = courses.ToList();
-            _logger.LogInformation($"{cl.Count()} courses will be inserted.");
+            var cl = new List<Course>();
+            foreach (var c in courses)
+            {
+                var course = new Course()
+                {
+                    Semester = c.Semester,
+                    Code = c.Code,
+                    Name = c.Name,
+                    EnglishName = c.EnglishName,
+                    DepartmentId = c.DepartmentId,
+                    Credit = c.Credit,
+                    Required = c.Required,
+                    Year = c.Year,
+                    Time = c.Time,
+                    ClassRoomNo = c.ClassRoomNo,
+                    Contents = c.Contents,
+                    ImportSignatureId = signature.Id,
+                };
+                course.Teachers = await ProcessTeacherString(c.Teachers!, signature);
+                cl.Add(course);
+            }
+
+            _logger.LogInformation($"{year}{semester} {dept} has {cl.Count()} courses.");
             courseList.AddRange(cl);
         }
 
-
         // Task4: Insert courses
-        _logger.LogInformation($"({year}{semester}) semester has {courseList.Count} courses will be inserted with signature {importSignature.Name}");
+        _logger.LogInformation($"Semester {year}{semester} has {courseList.Count} courses with signature {importSignature.Name} will be inserted.");
         await _courseRepo.AddRangeAsync(courseList, cancellationToken);
     }
-}
 
+    private async Task<ICollection<CourseTeacher>> ProcessTeacherString(ICollection<Teacher> teacher, ImportSignature s)
+    {
+        // Hint: 老師的英文名字可能會出錯
+        // 如：Mei H.C. Ho,Vincent Kuo,Hsiao-Hui Chen,Sun-Jen Huang,Hwa-Meei Liou, John S. Liu => 六位老師
+        // 如2: Yang, chuan-kai,Miao, Wei-Chung => 兩位老師
+        var regex = new Regex(",(?!\\s)");
+        string[] names = regex.Split(teacher.First().Name);
+        string[] englishNames = regex.Split(teacher.First().EnglishName!);
+        if (names.Length > englishNames.Length)
+        {
+            englishNames = teacher.First().EnglishName!.Split(',');
+        }
+
+        var correctTeachers = names
+            .Select(n => new Teacher()
+            {
+                Name = n.Trim(),
+                EnglishName = englishNames[names.ToList().IndexOf(n)].Trim(),
+                EnglishNameInNtustCourse = englishNames[names.ToList().IndexOf(n)].Trim(),
+                ImportSignatureId = s.Id
+            }).ToList();
+
+        for (int i = 0; i < correctTeachers.Count; i++)
+        {
+            var t =
+                await _teacherRepo.FirstOrDefaultAsync(new GetTeacherByNameSpec(correctTeachers[i].Name, correctTeachers[i].EnglishNameInNtustCourse, s.Id));
+            correctTeachers[i] = t ?? await _teacherRepo.AddAsync(correctTeachers[i]);
+        }
+
+        return correctTeachers.ConvertAll(t => new CourseTeacher()
+        {
+            TeacherId = t.Id
+        });
+    }
+}
--- src/Infrastructure/ReportGenerator/Request/Model/CourseResponse.cs
+++ src/Infrastructure/ReportGenerator/Request/Model/CourseResponse.cs
@@ -1,29 +1,55 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace AACSB.WebApi.Infrastructure.ReportGenerator.Request.Model;
 
 public class CourseResponse
 {
+    [AllowNull]
     public string Semester { get; set; } // 1111
+    [AllowNull]
     public string CourseNo { get; set; } // MB112A001
+    [AllowNull]
     public string CourseName { get; set; } // 經濟學(上)
+    [AllowNull]
     public string CourseTeacher { get; set; } // 莊育娟
+    [AllowNull]
     public string Dimension { get; set; } // ""
+    [AllowNull]
     public string CreditPoint { get; set; } // 3
+    [AllowNull]
     public string RequireOption { get; set; } // Required = R, Not required = E
+    [AllowNull]
     public string AllYear { get; set; } // Full Year = F, Half Year = H
+    [AllowNull]
     public int ChooseStudent { get; set; } // 42
+    [AllowNull]
     public string Restrict1 { get; set; } // 9999
+    [AllowNull]
     public string Restrict2 { get; set; } // 55
+    [AllowNull]
     public int ThreeStudent { get; set; } // 0
+    [AllowNull]
     public int AllStudent { get; set; } // 42
+    [AllowNull]
     public string NTURestrict { get; set; } // 0
+    [AllowNull]
     public string NTNURestrict { get; set; } // 0
+    [AllowNull]
     public string CourseTimes { get; set; } // 3
+    [AllowNull]
     public string PracticalTimes { get; set; } // 0
+    [AllowNull]
     public string ClassRoomNo { get; set; } // IB-608
+    [AllowNull]
     public string? ThreeNode { get; set; } // null
+    [AllowNull]
     public string Node { get; set; } // W2,W3,W4
+    [AllowNull]
     public string Contents { get; set; } // ""
+    [AllowNull]
     public int NTU_People { get; set; } // 0
+    [AllowNull]
     public int NTNU_People { get; set; } // 0
+    [AllowNull]
     public int AbroadPeople { get; set; } // 0
 }
