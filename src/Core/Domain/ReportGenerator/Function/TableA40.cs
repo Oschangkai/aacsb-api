0f9b28a621223c89cb73d39b249b6b710b18d716
Oschangkai <oschangkai@kaiyeee.tw>
2023-09-27 16:44:12 +0800

    Feat: Table A40

---
+++ src/Core/Domain/ReportGenerator/Function/TableA40.cs
@@ -0,0 +1,9 @@
+namespace AACSB.WebApi.Domain.ReportGenerator.Function;
+
+public class TableA40
+{
+    public string Department { get; set; }
+    public string CourseDegree { get; set; }
+    public string WorkType { get; set; }
+    public double Credit { get; set; }
+}
